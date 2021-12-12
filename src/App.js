cd76edae0fb279dfcb79fe31a18686564e82f367
mihaitrof <mihaitrof@gmail.com>
2021-12-12 22:59:48 +0200

    Regular counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/3-useState-array.js";
+import Setup from "./tutorial/1-useState/setup/5-useState-counter.js";
 import Final from "./tutorial/1-useState/final/2-useState-basics.js";
 
 function App() {
