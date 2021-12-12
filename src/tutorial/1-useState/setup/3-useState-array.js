e7dfbace660bf8ac8880bf8ba1f20a3c8488cf58
mihaitrof <mihaitrof@gmail.com>
2021-12-12 15:02:13 +0200

    remove each item separately

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,6 +3,10 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
   return (
     <>
       {people.map((person) => {
@@ -10,6 +14,7 @@ const UseStateArray = () => {
         return (
           <div key={id} className="item">
             <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
