a81359333affc2a3192f285f2dfa44cf446028f3
Indie Mike <indiemike01@gmail.com>
2021-12-27 20:08:18 +0200

    Custom hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Setup from "./tutorial/8-useContext/setup/1-context-api";
-// import Final from "./tutorial/6-useReducer/final";
+import Setup from "./tutorial/10-prop-types/setup/";
 
 function App() {
   return (
     <div className="container">
       <Setup />
-      {/* <Final /> */}
     </div>
   );
 }
